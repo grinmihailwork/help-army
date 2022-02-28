79b875e84a93b4d466ef63a2577f6111e6bfe0c5
AndrewMos <moskovecandrey@gmail.com>
2022-02-28 20:03:25 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Body />
+    <Wallet/>
+    </>
+  );
+}
+
+const Header = styled.div`
+
+`
+
+const Body = styled.div`
+
+`
+
+const Wallet = styled.div`
+
+`
+
+export default App;
